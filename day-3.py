be6f370c5ff5e807edc282ca4238d028ce65a9ec
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-17 23:38:04 +0100

    file

--- day-3.py
+++ day-3.py
@@ -1,2 +1,17 @@
+# --- working with data types: numbers, strings, lists and tuples
+
+string = "name" # items concatenated in double-qoute are reffered to ass string
+
+numbers = 300 # numbers are just figure either with or without decimal point
+
+lists = [10, balls, yellow, 450, 2.3] #list is a collection of ekements written in []
+
+tuples = (ade, bola, 55, blue) # tuples are similar to list but are writtne in (), they cant be modified after initializing unlike lists that can be editted always
+
+
+
+
+# --- mini project: write a python script to reverse a given string
+
 script = "Hello World"
 print(script[::-1])
