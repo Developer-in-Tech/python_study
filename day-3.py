f06bbfaf48e4459c4c7d0c43b2487d9f5f17c6ae
Developer-in-Tech <princeakinola05@gmail.com>
2023-05-18 20:43:31 +0100

    fike

--- day-3.py
+++ day-3.py
@@ -4,9 +4,9 @@ string = "name" # items concatenated in double-qoute are reffered to ass string
 
 numbers = 300 # numbers are just figure either with or without decimal point
 
-lists = [10, balls, yellow, 450, 2.3] #list is a collection of ekements written in []
+color_list = ["blue", "yellow", "red"] # list is a collection of ekements written in []
 
-tuples = (ade, bola, 55, blue) # tuples are similar to list but are writtne in (), they cant be modified after initializing unlike lists that can be editted always
+ball_tuples = ("fred", "hakimi", "vini") # tuples are similar to list but are writtne in (), they cant be modified after initializing unlike lists that can be editted always
 
 
 
